c95e6f047dbbaa7191a187d4fa15c9df85cf6be3
Raphayol <rapha@rapha-asus.(none)>
2014-04-07 22:23:38 +0200

    new app rails empty pushed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ActiveVerre::Application.config.secret_key_base = '2fee7e3d837df2251017753da31cd0f97d421505d50306523c6d026e9c5648a345b3b2c81ebf28924cb5ae74f65592afb2a9b42280d14c2dd65ed62f204ae4f4'
