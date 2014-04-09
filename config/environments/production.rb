bcf510257f49e4e56e3bdbebceaf9fb3f2bb9cdd
Raphayol <raphael.egeley@gmail.com>
2014-04-09 03:16:47 +0200

    config for production modified

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ ActiveVerre::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
   config.assets.precompile += ['active_admin.js', 'active_admin.css', 'active_admin/print.css']
   # Generate digests for assets URLs.
   config.assets.digest = true
