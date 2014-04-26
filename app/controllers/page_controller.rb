78f01e812695bb8a230ce65b04e582c5cd9daf35
Thibaut Assus <thibaut.assus@gmail.com>
2014-04-27 01:22:35 +0200

    Refactor a little

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,17 +1,2 @@
 class PageController < ApplicationController
-  def home
-    @titre = "Acceuil"
-  end
-
-  def contact
-    @titre = "Contacts"
-  end
-
-  def my_account
-    @titre = "Mon compte"
-  end
-  
-  def product
-    @titre = "Produit"
-  end
 end
--- app/controllers/produit_controller.rb
+++ app/controllers/produit_controller.rb
@@ -1,5 +1,5 @@
 class ProduitController < ApplicationController
-	def new
-      @produit = Produit.new
-   end
+  def new
+    @produit = Produit.new
+  end
 end
